70e246c8971cce0b85a342abe8db4189d773f79b
Ivo2313 <ivo.estevan@gmail.com>
2023-02-15 16:44:38 -0300

    fuente download

--- protur/src/common /buttoms/index.tsx
+++ protur/src/common /buttoms/index.tsx
@@ -12,15 +12,15 @@ export const Standarbuttom = (props: { name: string | number | boolean | ReactEl
 }
 
 export const IconButtomLeft = (props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }) => {
-  return <button className={`flex gap-2 items-center rounded-3xl border-solid border-[#CCCCCC] text-[#999999] bg-transparent`}>{props.name} <LinkBottom color="#999999" size="18px"/></button>;
+  return <button className={`flex gap-2 items-center rounded-3xl border-solid border-[#CCCCCC] text-[#999999] bg-transparent font-quicksandlight font-bold`}>{props.name} <LinkBottom color="#999999" size="18px"/></button>;
 
   
 }
 export const IconButtomRight = (props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }) => {
-  return <button className={`flex gap-2 items-center rounded-3xl border-solid border-[#CCCCCC] text-[#999999] bg-transparent`}><LinkPrevious color="#999999" size="18px"/> {props.name}</button>;
+  return <button className={`flex gap-2 items-center rounded-3xl border-solid border-[#CCCCCC] text-[#999999] bg-transparent font-quicksandlight font-bold`}><LinkPrevious color="#999999" size="18px"/> {props.name}</button>;
 }
 export const IconButtomRightDark = (props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | ReactFragment | ReactPortal | null | undefined }) => {
-  return <button className={`flex gap-2 items-center rounded-3xl border-solid border-[#CCCCCC] text-[#999999] bg-transparent activo`}>{props.name} <Briefcase color="white" size="18px"/></button>;
+  return <button className={`flex gap-2 items-center rounded-3xl border-solid border-[#CCCCCC] text-[#999999] bg-transparent activo font-quicksandlight font-bold`}>{props.name} <Briefcase color="white" size="18px"/></button>;
 }
 
 
