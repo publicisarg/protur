b55002b703dc5c5f47711686c92256707d7412c3
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-02-15 16:30:43 -0300

    Version popup al abrir imagen

--- protur/src/App.tsx
+++ protur/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { Download } from './containers/download'
 import { Footer } from './containers/footer'
 import { Header } from './containers/header'
 import { Main } from './containers/main'
@@ -11,7 +12,7 @@ function App() {
       <Header></Header> 
       <Main></Main>
       <Footer></Footer>
-     
+      {<Download imgsrc="https://cloudfront-us-east-1.images.arcpublishing.com/metroworldnews/55QHEOQRQBEPTF5LRJK57MDEP4.jpg"/>}
     </>
   )
 }
--- protur/src/containers/download/index.tsx
+++ protur/src/containers/download/index.tsx
@@ -1,12 +1,13 @@
 import logo from '../../../public/logoblack.svg'
 import { IconButtomLeft, IconButtomRight, IconButtomRightDark } from '../../common /buttoms'
+import { Close } from 'grommet-icons';
 
 export const Download = (props: any) => {
     return (
-      <div className="absolute m-auto inset-0 flex justify-center items-center z-50 bg-gray-400/50">
-        <div className="absolute drop-shadow-lg px-12 py-8 rounded-lg flex flex-col justify-center items-center bg-white">
+      <div className="absolute m-auto inset-0 flex justify-center items-center z-40 bg-gray-400/50">
+        <div className="absolute drop-shadow-lg px-12 py-8 rounded-lg flex flex-col z-50 justify-center items-center bg-white md:w-3/4">
           <img src={logo} className="pb-6 w-40"/>
-          <img className="rounded-lg" src={props.imgsrc} />
+          <img className="rounded-lg md:max-w-[70%]" src={props.imgsrc} />
           <div className="flex flex-col sm:flex-row justify-between w-full mt-6 gap-4 sm:gap-0">
             <IconButtomRight name={"Volver"}/>
             <div className="flex flex-col sm:flex-row gap-4">
