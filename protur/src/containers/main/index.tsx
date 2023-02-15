b18b0089a2f55c2b22426da73ed9f01862c58d54
jumperning <maxy.m.rodriguez@gmail.com>
2023-02-15 19:54:31 -0300

    set time cartel

--- protur/src/containers/main/index.tsx
+++ protur/src/containers/main/index.tsx
@@ -8,12 +8,12 @@ export const Main = () => {
     <div className='container mx-auto items-center'>
       <div className='my-[50px] text-center text-[32px]'>
         <p className='font-quicksandlight font-bold'>Encuentra un fondo de pantalla desde el que quieras trabajar</p>
-      </div>
+        </div>
       <Search></Search>
       <div className='border-solid border-t-2 py-9'>
         <ListFilter></ListFilter>
       </div>
-      <div className='grid grid-cols-3 gap-24'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-24'>
         <Griditems />
       </div>
     </div>
