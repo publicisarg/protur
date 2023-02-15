7c814b513c2857facc78c36b0d7959fc4234409a
Ivo Estevan <ivoestev@WKMZT2792DF9.global.publicisgroupe.net>
2023-02-15 15:30:44 -0300

    correccion  de estilos

--- protur/src/containers/main/index.tsx
+++ protur/src/containers/main/index.tsx
@@ -5,6 +5,9 @@ import { Search } from '../../components/filters/search'
 export const Main = () => {
   return (
     <>
+      <div className='my-[50px] text-center text-[32px]'>
+        <p>Encuentra un fondo de pantalla desde el que quieras trabajar</p>
+      </div>
       <Search></Search>
       <div className='border-solid border-t-2 pt-9'>
       <ListFilter></ListFilter>
