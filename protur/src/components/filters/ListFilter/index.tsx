39cbd62130fe0a5aec8f5bf63a36165fae77c126
jumperning <maxy.m.rodriguez@gmail.com>
2023-02-16 11:30:56 -0300

    transiciones

--- protur/src/components/filters/ListFilter/index.tsx
+++ protur/src/components/filters/ListFilter/index.tsx
@@ -1,62 +1,79 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import GridCard from '../../grid/GridCard';
 import { Search } from '../search';
 
 const categorias = ['Ciudades', 'Rutas', 'Pueblos', 'Naturaleza'];
 
-const contenido = [
-  {
-    categoria: 'Ciudades',
-    elementos: ['lugares/01.jpeg', 'lugares/02.jpeg', 'lugares/03.jpg', 'lugares/04.jpeg']
+const contenido = [  {    categoria: 'Ciudades',    elementos: ['lugares/01.jpeg', 'lugares/02.jpeg', 'lugares/03.jpg', 'lugares/04.jpeg']
   },
   {
     categoria: 'Rutas',
-    elementos:  ['lugares/04.jpeg', 'lugares/02.jpeg', 'lugares/03.jpg', 'lugares/01.jpeg']
+    elementos: ['lugares/04.jpeg', 'lugares/02.jpeg', 'lugares/03.jpg', 'lugares/01.jpeg']
   },
   {
     categoria: 'Pueblos',
-    elementos:  ['lugares/02.jpeg', 'lugares/03.jpg', 'lugares/04.jpeg', 'lugares/01.jpeg']
+    elementos: ['lugares/02.jpeg', 'lugares/03.jpg', 'lugares/04.jpeg', 'lugares/01.jpeg']
   },
   {
     categoria: 'Naturaleza',
-    elementos:  ['lugares/03.jpg', 'lugares/02.jpeg', 'lugares/04.jpeg', 'lugares/01.jpeg']
+    elementos: ['lugares/03.jpg', 'lugares/02.jpeg', 'lugares/04.jpeg', 'lugares/01.jpeg']
   }
 ];
 
 export const ListFilter = (props: any) => {
+
+
   const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('Ciudades');
   const [busqueda, setBusqueda] = useState('');
   const [isActive, setActive] = useState(false);
-  const handleCategoriaClick = (categoria:any) => {
+  const [transiciones, setTransiciones] = useState(false);
+  const [animation, setAnimation] = useState({ showAppearClass: false, transiciones: true });
+
+  const handleCategoriaClick = (categoria: any) => {
     setCategoriaSeleccionada(categoria);
     setActive(!isActive);
-  };
- 
-  
 
-  const contenidoFiltrado = contenido.filter((item) => item.categoria === categoriaSeleccionada);
+    // eliminar la clase "aparecer" después de la transición
+    setAnimation({ showAppearClass: false, transiciones: animation.transiciones });
+
+    // filtrar los elementos según la categoría seleccionada
+    const filteredElements = contenido.filter((element: any) => element.categoria === categoria);
+    setContenidoFiltrado(filteredElements);
+
+    // esperar un corto tiempo para que se complete la transición
+    setTimeout(() => {
+      // volver a agregar la clase "aparecer" después de renderizar los nuevos elementos
+      setAnimation({ showAppearClass: true, transiciones: animation.transiciones });
+    }, 500);
+  };
+
+  useEffect(() => {
+    setAnimation({ showAppearClass: animation.showAppearClass, transiciones: true });
+  }, [categoriaSeleccionada]);
+
+  const [contenidoFiltrado, setContenidoFiltrado] = useState(contenido.filter((item) => item.categoria === categoriaSeleccionada));
 
   return (
-    <div>
-    
+    <div className='listFilter'>
       <div className='grid px-5 md:p-0 grid-cols-2 lg:grid-cols-8 gap-6'>
         {categorias.map((categoria) => (
-         
-          <button className={`rounded-3xl border-solid border-[#CCCCCC] text-[#999999] bg-transparent ${categoriaSeleccionada === categoria ? 'activo' : ''}`} key={categoria} onClick={() => handleCategoriaClick(categoria)} >
+          <button
+            className={`rounded-3xl border-solid border-[#CCCCCC] text-[#999999] bg-transparent ${categoriaSeleccionada === categoria ? 'activo' : ''}`}
+            key={categoria}
+            onClick={() => handleCategoriaClick(categoria)}
+          >
             {categoria}
-            </button>
+          </button>
         ))}
       </div>
       {categoriaSeleccionada && (
-        
-        <ul className='p-5 md:p-0 grid md:grid-cols-2 lg:grid-cols-3 gap-24  my-10'>
-          {contenidoFiltrado[0].elementos.map((elemento) => (
-            <li key={elemento}>
-              <GridCard img={elemento} link={""} />
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  );
-}
+      <ul className={`p-5 md:p-0 grid md:grid-cols-2 lg:grid-cols-3 gap-24 my-10 elementos ${animation.showAppearClass ? 'aparecer' : ''} ${animation.transiciones ? 'transition-all duration-500' : ''}`}>
+        {contenidoFiltrado[0].elementos.map((elemento, index) => (
+          <li key={elemento} className={animation.showAppearClass ? 'aparecer' : ''} style={{ transitionDelay: `${index * 100}ms` }}>
+            <GridCard img={elemento} link={""} />
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+)};
