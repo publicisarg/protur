539200fde772aa06435ea926f9e28f063166f6f5
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-02-16 12:24:02 -0300

    correcciones pop up visualizacion de imagenes y paso a componente

--- protur/src/components/grid/GridCard.tsx
+++ protur/src/components/grid/GridCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Download } from '../../containers/download/normal';
+import { DownloadPopUp } from '../../containers/download/pop-up';
 import logo from '../../../public/logoblack.svg'
 import { IconButtomLeft, IconButtomRight, IconButtomRightDark } from '../../common /buttoms'
 import { Close } from 'grommet-icons';
@@ -30,19 +30,7 @@ function GridCard(props:any) {
       <img src={props.img} alt={props.alt} onClick={handleClick} className="rounded-lg h-60 object-cover w-full" />
 
       {showPopup && (
-        <div className="popup h-full grid justify-items-center items-center overflow-hidden fixed top-0 left-0 right-0 z-50 bg-white">
-          <div className="container px-12 py-8 rounded-lg flex flex-col z-50 justify-center items-center bg-white">
-            <img src={logo} className="pb-6 w-40" />
-            <img className="rounded-lg md:max-w-[70%] w-full" src={props.img} />
-            <div className="flex flex-col sm:flex-row justify-between w-full mt-6 gap-4 sm:gap-0">
-              <div onClick={handleClose}><IconButtomRight name={"Volver"} /></div>
-              <div className="flex flex-col sm:flex-row gap-4">
-                <a href={props.imgsrc} download={props.img} onClick={handleDescarga}><IconButtomLeft name={"Descargar fondo"} /></a>
-                <a href={props.linksrc}><IconButtomRightDark name={"Trabajar aquí"} /></a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <DownloadPopUp imgsrc={props.img} img={props.alt} handleFunc={handleDescarga} setShowPopup={setShowPopup} linksrc={props.linksrc}/>
       )}
     </>
   );
--- protur/src/containers/download/pop-up.tsx
+++ protur/src/containers/download/pop-up.tsx
@@ -3,9 +3,33 @@ import { IconButtomLeft, IconButtomRight, IconButtomRightDark } from '../../comm
 import { Download } from './normal'
 
 export const DownloadPopUp = (props: any) => {
+
+  const handleDescarga = () => {
+    const descarga = document.createElement('a');
+    descarga.href = props.imgsrc;
+    descarga.download = 'Paisaje-Argentina.jpg';
+    document.body.appendChild(descarga);
+    descarga.click();
+    document.body.removeChild(descarga);
+  }
+
+  function handleClose() {
+    props.setShowPopup(false);
+  }
+
     return (
-      <div className="absolute m-auto inset-0 flex justify-center items-center z-40 bg-gray-400/50">
-        <Download imgsrc={props.imgsrc} className="absolute drop-shadow-lg md:w-3/4"/>
-      </div> 
+      <div className="popup h-full grid justify-items-center items-center overflow-hidden fixed top-0 left-0 right-0 z-50 bg-white">
+        <div className="container px-12 py-8 rounded-lg flex flex-col z-50 justify-center items-center bg-white">
+          <img src={logo} className="pb-6 w-40" />
+          <img className="rounded-lg md:max-h-[50vh] w-auto" src={props.imgsrc} />
+          <div className="flex flex-col sm:flex-row justify-between w-full mt-6 gap-4 sm:gap-0">
+            <div onClick={handleClose}><IconButtomRight name={"Volver"} /></div>
+            <div className="flex flex-col sm:flex-row gap-4">
+              <a onClick={handleDescarga}><IconButtomLeft name={"Descargar fondo"} /></a>
+              <a href={props.linksrc}><IconButtomRightDark name={"Trabajar aquí"} /></a>
+            </div>
+          </div>
+        </div>
+      </div>
     )
 }
