7cf80ac523ba1583601572b9fcb5700b1f79c91e
Julian Di pietrantonio <juldipie@WKMZT907C970.global.publicisgroupe.net>
2023-02-16 13:06:34 -0300

    correccion estilos

--- protur/src/containers/download/pop-up.tsx
+++ protur/src/containers/download/pop-up.tsx
@@ -19,7 +19,7 @@ export const DownloadPopUp = (props: any) => {
 
     return (
       <div className="popup h-[100vh] grid justify-items-center items-center overflow-hidden fixed top-0 left-0 right-0 z-50 bg-white">
-        <div className="container px-12 py-8 rounded-lg flex flex-col z-50 justify-center items-center bg-white">
+        <div className="container px-12 py-8 rounded-lg flex flex-col z-50 justify-center items-center bg-white max-w-screen-lg">
           <img src={logo} className="pb-6 w-40" />
           <img className="rounded-lg md:max-h-[50vh] w-auto" src={props.imgsrc} />
           <div className="flex flex-col sm:flex-row justify-between w-full mt-6 gap-4 sm:gap-0">
